ad562b05466d82702d72ceb55659167fe6aedfa2
Ichimura-Riku <c1103865@planet.kanazawa-it.ac.jp>
2023-08-25 15:01:27 +0900

    app barを実装完了

---
+++ app/src/main/java/com/example/todo_app_jpc/MainActivity.kt
@@ -0,0 +1,103 @@
+package com.example.todo_app_jpc
+import android.annotation.SuppressLint
+import android.graphics.Color
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.todo_app_jpc.ui.theme.Todo_app_jpcTheme
+
+class MainActivity : ComponentActivity() {
+    @ExperimentalMaterial3Api
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Todo_app_jpcTheme {
+                Scf()
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Column (modifier = modifier,
+
+        ){
+        Text(
+            text = "Hello $name!",
+            modifier = modifier,
+            color = MaterialTheme.colorScheme.onPrimaryContainer
+
+        )
+    }
+}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@ExperimentalMaterial3Api
+@Composable
+fun Scf(){
+    val topAppBarColors = TopAppBarDefaults.centerAlignedTopAppBarColors (
+        containerColor = MaterialTheme.colorScheme.background
+
+    )
+    Todo_app_jpcTheme {
+
+
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = {
+                        Text(
+                            text = "top app bar",
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis,
+                            color = MaterialTheme.colorScheme.onPrimaryContainer
+                        )
+                    },
+                    colors = topAppBarColors,
+                )
+            },
+            content = {
+                Surface(
+                    modifier = Modifier,
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Greeting("Android")
+                    }
+                }
+            },
+        )
+    }
+}
+
+
+@ExperimentalMaterial3Api
+@Preview(showBackground = true)
+@Composable
+fun ScfPreview() {
+    Todo_app_jpcTheme {
+        Scf()
+    }
+}
+
