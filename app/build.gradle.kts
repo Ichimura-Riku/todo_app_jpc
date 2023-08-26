faed0889cc2b77f5ad303dca8c126b7e82266647
Ichimura-Riku <c1103865@planet.kanazawa-it.ac.jp>
2023-08-26 20:28:08 +0900

    Hiltを支える体制にした

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.todo_app_jpc"
-        minSdk = 24
+        minSdk = 25
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
---
+++ app/src/main/java/com/example/todo_app_jpc/MainApplication.kt
@@ -0,0 +1,9 @@
+package com.example.todo_app_jpc
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MainApplication: Application()  {
+
+}
