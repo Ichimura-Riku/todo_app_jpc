32a9823c0a651a60a3c919f5e4820d41a601f1ef
Ichimura-Riku <c1103865@planet.kanazawa-it.ac.jp>
2023-08-27 12:06:22 +0900

    databaseの構築

---
+++ app/src/main/java/com/example/todo_app_jpc/Room/InventoryDatabase.kt
@@ -0,0 +1,26 @@
+package com.example.todo_app_jpc.Room
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Todo::class], version = 1, exportSchema = false)
+abstract class InventoryDatabase : RoomDatabase() {
+
+    abstract fun TodoDao(): TodoDao
+
+    companion object {
+        @Volatile
+        private var Instance: InventoryDatabase? = null
+
+        fun getDatabase(context: Context): InventoryDatabase {
+            // if the Instance is not null, return it, otherwise create a new database instance.
+            return Instance ?: synchronized(this) {
+                Room.databaseBuilder(context, InventoryDatabase::class.java, "todo_database")
+                    .build()
+                    .also { Instance = it }
+            }
+        }
+    }
+}
