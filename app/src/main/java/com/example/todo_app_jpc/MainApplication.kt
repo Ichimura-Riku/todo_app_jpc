7b0de9d422ebba8dde830224006d1393320e0427
Ichimura-Riku <c1103865@planet.kanazawa-it.ac.jp>
2023-09-01 17:40:41 +0900

    Applicationの追記

--- app/src/main/java/com/example/todo_app_jpc/MainApplication.kt
+++ app/src/main/java/com/example/todo_app_jpc/MainApplication.kt
@@ -1,7 +1,19 @@
 package com.example.todo_app_jpc
 
 import android.app.Application
+import com.example.todo_app_jpc.Data.AppContainer
+import com.example.todo_app_jpc.Data.AppDataContainer
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class MainApplication: Application()
+class MainApplication: Application(){
+    // 他のクラスが依存関係を取得するために使用するAppContainerインスタンス
+
+    lateinit var container: AppContainer
+    override fun onCreate() {
+        super.onCreate()
+        container = AppDataContainer(this)
+    }
+
+}
