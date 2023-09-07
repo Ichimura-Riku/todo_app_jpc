bf7326430b73529b1f5ec7708a4614c19be8cdf8
Ichimura-Riku <c1103865@planet.kanazawa-it.ac.jp>
2023-09-07 18:50:42 +0900

    Todoアプリ本線のproviderを作成

---
+++ app/src/main/java/com/example/todo_app_jpc/ui/TodoAppViewModelProvider.kt
@@ -0,0 +1,24 @@
+package com.example.todo_app_jpc.ui
+
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.todo_app_jpc.MainApplication
+import com.example.todo_app_jpc.ui.todo.TodoEntryViewModel
+
+object TodoAppViewModelProvider {
+    val Factory = viewModelFactory {
+        initializer {
+            TodoEntryViewModel(
+                this.createSavedStateHandle(),
+                todoApplication().todoContainer.todoRepository
+            )
+        }
+    }
+}
+
+fun CreationExtras.todoApplication(): MainApplication =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MainApplication)
