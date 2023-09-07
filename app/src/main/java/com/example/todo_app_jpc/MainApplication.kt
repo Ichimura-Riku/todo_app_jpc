5562c1e80edcfbec360721334c4636ef273da084
Ichimura-Riku <c1103865@planet.kanazawa-it.ac.jp>
2023-09-07 18:51:35 +0900

    Todoアプリ本線のためのcontainer設定に変更

--- app/src/main/java/com/example/todo_app_jpc/MainApplication.kt
+++ app/src/main/java/com/example/todo_app_jpc/MainApplication.kt
@@ -1,7 +1,9 @@
 package com.example.todo_app_jpc
 
 import android.app.Application
-import com.example.todo_app_jpc.ItemData.AppContainer
+import com.example.todo_app_jpc.Data.AppContainer
+import com.example.todo_app_jpc.Data.TodoAppDataContainer
+
 import com.example.todo_app_jpc.ItemData.AppDataContainer
 import dagger.hilt.android.HiltAndroidApp
 
@@ -10,10 +12,12 @@ import dagger.hilt.android.HiltAndroidApp
 class MainApplication: Application(){
     // 他のクラスが依存関係を取得するために使用するAppContainerインスタンス
 
-    lateinit var container: AppContainer
+//    lateinit var container: AppContainer
+    lateinit var todoContainer: AppContainer
     override fun onCreate() {
         super.onCreate()
-        container = AppDataContainer(this)
+//        container = AppDataContainer(this)
+        todoContainer = TodoAppDataContainer(this)
     }
 
 }
