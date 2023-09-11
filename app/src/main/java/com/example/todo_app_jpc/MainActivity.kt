2a562076c02b3d1bdd36e8b6e3ea6350ef9772f9
Ichimura-Riku <c1103865@planet.kanazawa-it.ac.jp>
2023-09-11 17:11:47 +0900

    mainActivityをリファクタリング

--- app/src/main/java/com/example/todo_app_jpc/MainActivity.kt
+++ app/src/main/java/com/example/todo_app_jpc/MainActivity.kt
@@ -1,5 +1,3 @@
-
-
 package com.example.todo_app_jpc
 
 import android.annotation.SuppressLint
@@ -7,19 +5,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 
-import androidx.compose.foundation.layout.fillMaxSize
 
 import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 
-import androidx.compose.ui.Modifier
+
 import androidx.core.view.WindowCompat
 import com.example.todo_app_jpc.ui.home.MyAppView
 
 import com.example.todo_app_jpc.ui.theme.Todo_app_jpcTheme
-import dagger.hilt.android.AndroidEntryPoint
 
 
 //@AndroidEntryPoint
