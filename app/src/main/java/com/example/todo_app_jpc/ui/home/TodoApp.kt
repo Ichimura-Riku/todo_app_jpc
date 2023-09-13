68b849eb791448fe18ef0a6b04bc230c2adcbb25
Ichimura-Riku <c1103865@planet.kanazawa-it.ac.jp>
2023-09-13 14:16:48 +0900

    add: fabのアクションでbottomSheetを実装

--- app/src/main/java/com/example/todo_app_jpc/ui/home/TodoApp.kt
+++ app/src/main/java/com/example/todo_app_jpc/ui/home/TodoApp.kt
@@ -1,51 +1,41 @@
 package com.example.todo_app_jpc.ui.home
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.todo_app_jpc.R
-import com.example.todo_app_jpc.data.TodoEntity
-
 import com.example.todo_app_jpc.ui.TodoAppViewModelProvider
 import com.example.todo_app_jpc.ui.theme.Todo_app_jpcTheme
 import com.example.todo_app_jpc.ui.todo.TodoEntryBody
@@ -63,6 +53,9 @@ fun MyAppView() {
     val topAppBarColors = TopAppBarDefaults.centerAlignedTopAppBarColors(
         containerColor = MaterialTheme.colorScheme.background
     )
+    val sheetState = rememberModalBottomSheetState()
+    val scope = rememberCoroutineScope()
+    var showBottomSheet by remember { mutableStateOf(false) }
     Todo_app_jpcTheme {
         Scaffold(
             topBar = {
@@ -111,7 +104,7 @@ fun MyAppView() {
                             }
                         }
                         FloatingActionButton(
-                            onClick = { /*TODO*/ }, modifier = Modifier.padding(10.dp)
+                            onClick = { showBottomSheet = true }, modifier = Modifier.padding(10.dp)
                         ) {
                             Icon(Icons.Rounded.Add, "localized description")
                         }
@@ -130,23 +123,44 @@ fun MyAppView() {
                         horizontalAlignment = Alignment.CenterHorizontally,
                         verticalArrangement = Arrangement.Center
                     ) {
-                        TodoEntryBody(
-                            todoState = viewModel.todoUiState.todoState,
-                            onTodoValueChange = viewModel::updateTodoState,
-                            onSaveClick = {
-                                // Note: If the user rotates the screen very fast, the operation may get cancelled
-                                // and the item may not be saved in the Database. This is because when config
-                                // change occurs, the Activity will be recreated and the rememberCoroutineScope will
-                                // be cancelled - since the scope is bound to composition.
-                                coroutineScope.launch {
-                                    viewModel.adventTodo()
+                        if (showBottomSheet) {
+                            ModalBottomSheet(
+                                onDismissRequest = {
+                                    showBottomSheet = false
+                                },
+                                sheetState = sheetState
+                            ) {
+                                // Sheet content
+                                TodoEntryBody(
+                                    todoState = viewModel.todoUiState.todoState,
+                                    onTodoValueChange = viewModel::updateTodoState,
+                                    onSaveClick = {
+                                        // Note: If the user rotates the screen very fast, the operation may get cancelled
+                                        // and the item may not be saved in the Database. This is because when config
+                                        // change occurs, the Activity will be recreated and the rememberCoroutineScope will
+                                        // be cancelled - since the scope is bound to composition.
+                                        coroutineScope.launch {
+                                            viewModel.adventTodo()
 //                                    Todo: ぱっと見、バックアクションのナビゲーションに関する処理だから一旦放置
 //                                    navigateBack()
-                                }
-                            },
-                        )
+                                        }
+                                        scope.launch { sheetState.hide() }.invokeOnCompletion {
+                                            if (!sheetState.isVisible) {
+                                                showBottomSheet = false
+                                            }
+                                        }
+
+                                    },
+                                )
+
+
+                            }
+                        }
+
                     }
+                    
                 }
+                MainBody()
             },
 //            content = {
 //                val viewModel: TodoEntryViewModel = viewModel(factory = AppViewModelProvider.Factory)
@@ -168,6 +182,8 @@ fun MyAppView() {
         )
     }
 }
+
+
 
 
 @OptIn(ExperimentalMaterial3Api::class)
