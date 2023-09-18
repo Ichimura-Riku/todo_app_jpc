bc96dbc0de28f271d7f463c54ec1fc1aae615c79
Ichimura-Riku <c1103865@planet.kanazawa-it.ac.jp>
2023-09-18 22:25:24 +0900

    [change]navGraphのroute指定でHomeDestinationを適応

--- app/src/main/java/com/example/todo_app_jpc/ui/navigation/TodoNavGraph.kt
+++ app/src/main/java/com/example/todo_app_jpc/ui/navigation/TodoNavGraph.kt
@@ -5,6 +5,7 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.example.todo_app_jpc.ui.home.HomeDestination
 import com.example.todo_app_jpc.ui.home.MyAppView
 
 // ここではnavigationで画面遷移させるcomposeをそれぞれ設定する
@@ -14,8 +15,8 @@ fun TodoNavHost(
     navController: NavHostController,
     modifier: Modifier = Modifier,
 ) {
-    NavHost(navController = navController, startDestination = "home", modifier = modifier) {
-        composable(route = "home") { //　home -> HomeDestination.route
+    NavHost(navController = navController, startDestination = HomeDestination.route, modifier = modifier) {
+        composable(route = HomeDestination.route) {
             MyAppView(modifier = modifier)
         }
     }
