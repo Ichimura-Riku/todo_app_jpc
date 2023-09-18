293f27fc63f55d4eb2633bdd293b181f6583ec27
Ichimura-Riku <c1103865@planet.kanazawa-it.ac.jp>
2023-09-18 22:39:37 +0900

    [add]TodoDetailScreenの画面レイアウトをとりあえず組んでみた

--- app/src/main/java/com/example/todo_app_jpc/ui/todo/TodoDetailScreen.kt
+++ app/src/main/java/com/example/todo_app_jpc/ui/todo/TodoDetailScreen.kt
@@ -1,5 +1,76 @@
 package com.example.todo_app_jpc.ui.todo
 
-class TodoDetailScreen {
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.unit.dp
+import com.example.todo_app_jpc.R
+import com.example.todo_app_jpc.ui.home.TodoAppBar
 
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailScreen(
+    modifier: Modifier = Modifier.padding(10.dp)
+) {
+    MaterialTheme {
+        Scaffold(
+            topBar = { TodoAppBar(topBarText = "") },
+
+
+        ){ innerPadding -> TodoDetailBody(modifier = modifier.padding(innerPadding))
+
+        }
+    }
 }
+
+@Composable
+fun TodoDetailBody(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.padding(10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top,
+
+
+    ) {
+        TodoDetails()
+    }
+}
+
+@Composable
+fun TodoDetails(
+    modifier: Modifier = Modifier
+){
+    Card(
+        modifier = modifier,
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(dimensionResource(id = R.dimen.padding_medium)),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+        ) {
+            Text(text = "title")
+            Divider()
+
+
+        }
+    }
+}
+
