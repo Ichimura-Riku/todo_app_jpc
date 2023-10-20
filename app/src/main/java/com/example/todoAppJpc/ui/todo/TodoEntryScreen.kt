d861ccf9b6feb77deaafb1982b6ade3aac34ca96
Ichimura-Riku <c1103865@planet.kanazawa-it.ac.jp>
2023-10-20 09:31:42 +0900

    add timePickerをUIに仮導入

--- app/src/main/java/com/example/todoAppJpc/ui/todo/TodoEntryScreen.kt
+++ app/src/main/java/com/example/todoAppJpc/ui/todo/TodoEntryScreen.kt
@@ -1,12 +1,17 @@
 package com.example.todoAppJpc.ui.todo
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.DatePicker
@@ -16,19 +21,25 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TimePicker
 import androidx.compose.material3.rememberDatePickerState
+import androidx.compose.material3.rememberTimePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.todoAppJpc.R
 import java.time.Instant
+import java.util.Calendar
 
 
 @Composable
@@ -81,7 +92,7 @@ fun TodoEntryBody(
                         contentDescription = "Localized description",
                     )
                 }
-                IconButton(onClick = { viewModel.setShowDatePicker(!viewModel.getShowDatePicker()) }) {
+                IconButton(onClick = { viewModel.setShowTimePicker(!viewModel.getShowTimePicker()) }) {
                     Icon(
                         painterResource(id = R.drawable.round_swap_vert_24),
                         contentDescription = "Localized description",
@@ -112,6 +123,7 @@ fun TodoInputForm(
     val datePickerState = rememberDatePickerState(
         initialSelectedDateMillis = Instant.now().toEpochMilli()
     )
+    val state = rememberTimePickerState()
     Column(
         modifier = modifier,
         verticalArrangement = Arrangement.spacedBy(16.dp),
@@ -135,6 +147,20 @@ fun TodoInputForm(
         if (viewModel.getShowDatePicker()) {
 
             DatePickerComponent(viewModel = viewModel)
+        }
+        if (viewModel.getShowTimePicker()) {
+            TimePickerDialog(
+                onCancel = { viewModel.setShowTimePicker(false) },
+                onConfirm = {
+                    val cal = Calendar.getInstance()
+                    cal.set(Calendar.HOUR_OF_DAY, state.hour)
+                    cal.set(Calendar.MINUTE, state.minute)
+                    cal.isLenient = false
+                    viewModel.setShowTimePicker(false)
+                },
+            ) {
+                TimePicker(state = state)
+            }
         }
     }
 }
@@ -188,5 +214,61 @@ fun Material3DatePickerDialogComponent(
         modifier = modifier,
     ) {
         DatePicker(state = datePickerState)
+    }
+}
+
+@Composable
+fun TimePickerDialog(
+    title: String = "Select Time",
+    onCancel: () -> Unit,
+    onConfirm: () -> Unit,
+    toggle: @Composable () -> Unit = {},
+    content: @Composable () -> Unit,
+) {
+    Dialog(
+        onDismissRequest = onCancel,
+        properties = DialogProperties(
+            usePlatformDefaultWidth = false
+        ),
+    ) {
+        Surface(
+            shape = MaterialTheme.shapes.extraLarge,
+            tonalElevation = 6.dp,
+            modifier = Modifier
+                .width(IntrinsicSize.Min)
+                .height(IntrinsicSize.Min)
+                .background(
+                    shape = MaterialTheme.shapes.extraLarge,
+                    color = MaterialTheme.colorScheme.surface
+                ),
+        ) {
+            toggle()
+            Column(
+                modifier = Modifier.padding(24.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 20.dp),
+                    text = title,
+                    style = MaterialTheme.typography.labelMedium
+                )
+                content()
+                Row(
+                    modifier = Modifier
+                        .height(40.dp)
+                        .fillMaxWidth()
+                ) {
+                    Spacer(modifier = Modifier.weight(1f))
+                    TextButton(
+                        onClick = onCancel
+                    ) { Text("Cancel") }
+                    TextButton(
+                        onClick = onConfirm
+                    ) { Text("OK") }
+                }
+            }
+        }
     }
 }
