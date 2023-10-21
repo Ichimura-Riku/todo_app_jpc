a5d19946577642160df369ad13c5be2abf44fc6a
Ichimura-Riku <c1103865@planet.kanazawa-it.ac.jp>
2023-10-21 19:41:39 +0900

    change datePickerStateをviewModelからの値に変更

--- app/src/main/java/com/example/todoAppJpc/ui/components/DatePicker.kt
+++ app/src/main/java/com/example/todoAppJpc/ui/components/DatePicker.kt
@@ -1,31 +1,28 @@
 package com.example.todoAppJpc.ui.components
 
+import android.util.Log
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.DatePickerState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.example.todoAppJpc.ui.todo.TodoEntryViewModel
-import java.time.Instant
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DatePickerComponent(
     viewModel: TodoEntryViewModel,
 ) {
-    val datePickerState = rememberDatePickerState(
-        initialSelectedDateMillis = Instant.now().toEpochMilli(),
-    )
     if (viewModel.getShowDatePicker()) {
         Material3DatePickerDialogComponent(
-            datePickerState = datePickerState,
+            datePickerState = viewModel.datePickerState,
             closePicker = {
                 viewModel.setShowDatePicker(false)
                 viewModel.setShowTimePicker(true)
+                Log.d("debug-----", "${viewModel.datePickerState.selectedDateMillis}")
             },
         )
     }
