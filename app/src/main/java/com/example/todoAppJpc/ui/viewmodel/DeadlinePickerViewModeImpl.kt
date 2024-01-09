8b1732df5b54872ce19fa245a09b70f3a78a8946
Ichimura-Riku <c1103865@planet.kanazawa-it.ac.jp>
2024-01-09 11:58:09 +0900

    add updateDeadlineUiState(Impl)

--- app/src/main/java/com/example/todoAppJpc/ui/viewmodel/DeadlinePickerViewModeImpl.kt
+++ app/src/main/java/com/example/todoAppJpc/ui/viewmodel/DeadlinePickerViewModeImpl.kt
@@ -1,11 +1,68 @@
 package com.example.todoAppJpc.ui.viewmodel
 
+import android.util.Log
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.todoAppJpc.utils.deadline.viewModel.DatePickerViewModel
 import com.example.todoAppJpc.utils.deadline.viewModel.DeadlinePickerViewModel
 import com.example.todoAppJpc.utils.deadline.viewModel.TimePickerViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class DeadlinePickerViewModelImpl(
-    override var _datePickerViewModel: DatePickerViewModel,
-    override var _timePickerViewModel: TimePickerViewModel,
-) : ViewModel(), DeadlinePickerViewModel
+    private var _datePickerViewModel: DatePickerViewModel,
+    private var _timePickerViewModel: TimePickerViewModel,
+) : ViewModel(), DeadlinePickerViewModel {
+    override val datePickerViewModel get() = _datePickerViewModel
+    override val timePickerViewModel get() = _timePickerViewModel
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    override suspend fun updateDeadlineUiState(): String {
+        /**
+         * Chipを表示させるかどうかを判定する
+         * 一旦初期値を確認したい
+         * 初期値で比較して、もし何も入力されていないなら表示されない。
+         * 入力されているのであれば表示する文字列を返す
+         * 締切日時が入力されるのであれば必ず日付が入力されるんで、日付があるかどうかだけ確認すればいい
+         */
+
+        val userLocale = Locale.getDefault()
+        val cal = Calendar.getInstance()
+
+        val timeFormatter = SimpleDateFormat("HH:mm", userLocale)
+        val timeState = _timePickerViewModel.timePickerState
+        cal.set(Calendar.HOUR_OF_DAY, timeState.hour)
+        cal.set(Calendar.MINUTE, timeState.minute)
+        cal.isLenient = false
+        val timeUiState =
+            if (_datePickerViewModel.datePickerState.selectedDateMillis!! > 0L) timeFormatter.format(
+                cal.time
+            ) else ""
+        val dateFormatter = SimpleDateFormat("MM/dd(EEE)", userLocale)
+
+        try {
+            val setCalApply = viewModelScope.async(Dispatchers.IO) {
+                val dateState = _datePickerViewModel.datePickerState
+                cal.apply {
+                    timeInMillis = dateState.selectedDateMillis!!
+                }
+            }.await()
+            setCalApply
+        } catch (e: Exception) {
+            Log.e("getDeadlineUiState() is error", "$e")
+        }
+
+        val dateUiState =
+            if (_datePickerViewModel.datePickerState.selectedDateMillis!! > 0L) dateFormatter.format(
+                cal.time
+            ) else ""
+
+        return "$dateUiState $timeUiState"
+
+    }
+
+}
