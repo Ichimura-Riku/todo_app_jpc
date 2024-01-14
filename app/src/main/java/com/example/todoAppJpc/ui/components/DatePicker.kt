bb1d2628bbaa835c412fb8280a7936e15b150fd0
Ichimura-Riku <c1103865@planet.kanazawa-it.ac.jp>
2024-01-14 16:17:02 +0900

    change DatePickerをstateFlowの変更に対応

--- app/src/main/java/com/example/todoAppJpc/ui/components/DatePicker.kt
+++ app/src/main/java/com/example/todoAppJpc/ui/components/DatePicker.kt
@@ -7,29 +7,15 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import com.example.todoAppJpc.utils.deadline.viewModel.DeadlinePickerViewModel
 import kotlinx.coroutines.async
 
-//@OptIn(ExperimentalMaterial3Api::class)
-//@Composable
-//fun DatePickerComponent(
-//
-//) {
-//    Material3DatePickerDialogComponent(
-//        deadlineUiState = deadlineUiState,
-//        updateDeadlineUiViewState = updateDeadlineUiViewState,
-//        showDatePickerMutableState = showDatePickerMutableState,
-//        showTimePickerMutableState = showTimePickerMutableState,
-//        rememberDatePickerState = rememberDatePickerState,
-//    )
-//}
-
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DatePickerComponent(
-    title: String = "Set Date",
     deadlinePickerViewModel: DeadlinePickerViewModel,
     setChipView: suspend () -> Unit = {},
     modifier: Modifier
@@ -37,11 +23,9 @@ fun DatePickerComponent(
     val scope = rememberCoroutineScope()
     val timePickerViewModel = deadlinePickerViewModel.timePickerViewModel
     val datePickerViewModel = deadlinePickerViewModel.datePickerViewModel
-    var showDatePickerState = datePickerViewModel.showDatePicker
-    var showTimePickerState = timePickerViewModel.showTimePicker
     val hideDatePicker = { datePickerViewModel.setShowDatePicker(false) }
     val showTimePicker = { timePickerViewModel.setShowTimePicker(true) }
-    val datePickerState = datePickerViewModel.datePickerState
+    val datePickerState = datePickerViewModel.datePickerState.collectAsState()
     val datePickerStateSet = {
         val result = scope.async {
             setChipView()
@@ -85,6 +69,6 @@ fun DatePickerComponent(
         },
         modifier = modifier,
     ) {
-        DatePicker(state = datePickerState)
+        DatePicker(state = datePickerState.value)
     }
 }
