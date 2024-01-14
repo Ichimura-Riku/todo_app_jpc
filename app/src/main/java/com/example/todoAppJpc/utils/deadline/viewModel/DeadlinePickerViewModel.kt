5eb8bc99afa083ef34a43c2d0b7158ee61ce09a0
Ichimura-Riku <c1103865@planet.kanazawa-it.ac.jp>
2024-01-15 00:47:29 +0900

    clean 余計なアノテーション削除

--- app/src/main/java/com/example/todoAppJpc/utils/deadline/viewModel/DeadlinePickerViewModel.kt
+++ app/src/main/java/com/example/todoAppJpc/utils/deadline/viewModel/DeadlinePickerViewModel.kt
@@ -1,8 +1,6 @@
 package com.example.todoAppJpc.utils.deadline.viewModel
 
-import androidx.compose.material3.ExperimentalMaterial3Api
 
-@OptIn(ExperimentalMaterial3Api::class)
 interface DeadlinePickerViewModel {
     val datePickerViewModel: DatePickerViewModel
     val timePickerViewModel: TimePickerViewModel
