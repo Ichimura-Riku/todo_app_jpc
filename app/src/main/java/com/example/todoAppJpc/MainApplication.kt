dca919a023cd34c856f293534d6540778e16f951
Ichimura-Riku <c1103865@planet.kanazawa-it.ac.jp>
2024-01-14 21:15:12 +0900

    change DatabaseにHiltModuleを追加

--- app/src/main/java/com/example/todoAppJpc/MainApplication.kt
+++ app/src/main/java/com/example/todoAppJpc/MainApplication.kt
@@ -1,20 +1,7 @@
 package com.example.todoAppJpc
 
 import android.app.Application
-import com.example.todoAppJpc.data.TodoAppContainer
-import com.example.todoAppJpc.data.TodoAppDataContainer
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MainApplication : Application() {
-    // 他のクラスが依存関係を取得するために使用するAppContainerインスタンス
-
-    //    lateinit var container: AppContainer
-    // イラン
-    lateinit var todoContainer: TodoAppContainer
-    override fun onCreate() {
-        super.onCreate()
-//        container = AppDataContainer(this)
-        todoContainer = TodoAppDataContainer(this)
-    }
-}
+class MainApplication : Application()
