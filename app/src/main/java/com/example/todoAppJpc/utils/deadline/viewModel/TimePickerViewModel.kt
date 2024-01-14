c127012932c21da70677b117682cc4f32c548217
Ichimura-Riku <c1103865@planet.kanazawa-it.ac.jp>
2024-01-14 16:08:25 +0900

    change timePickerの値をstateFlowに変更

--- app/src/main/java/com/example/todoAppJpc/utils/deadline/viewModel/TimePickerViewModel.kt
+++ app/src/main/java/com/example/todoAppJpc/utils/deadline/viewModel/TimePickerViewModel.kt
@@ -2,15 +2,16 @@ package com.example.todoAppJpc.utils.deadline.viewModel
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.TimePickerState
+import kotlinx.coroutines.flow.StateFlow
 
 @OptIn(ExperimentalMaterial3Api::class)
 interface TimePickerViewModel {
 
     // 時間の値を取得する
-    val timePickerState: TimePickerState
+    val timePickerState: StateFlow<TimePickerState>
 
     // 時間選択ダイアログを表示するかどうかを取得する
-    val showTimePicker: Boolean
+    val showTimePicker: StateFlow<Boolean>
 
     // 時間の値をセットする
     fun setTimePickerState(value: TimePickerState)
