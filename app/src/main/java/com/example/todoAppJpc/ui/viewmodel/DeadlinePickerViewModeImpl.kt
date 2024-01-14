a7f21f02349b5d757a1a05bd5317d1304615de78
Ichimura-Riku <c1103865@planet.kanazawa-it.ac.jp>
2024-01-14 16:04:26 +0900

    remove inputChipState

--- app/src/main/java/com/example/todoAppJpc/ui/viewmodel/DeadlinePickerViewModeImpl.kt
+++ app/src/main/java/com/example/todoAppJpc/ui/viewmodel/DeadlinePickerViewModeImpl.kt
@@ -1,7 +1,9 @@
 package com.example.todoAppJpc.ui.viewmodel
 
 import android.util.Log
+import androidx.compose.material3.DatePickerState
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TimePickerState
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.todoAppJpc.utils.deadline.viewModel.DatePickerViewModel
@@ -9,6 +11,8 @@ import com.example.todoAppJpc.utils.deadline.viewModel.DeadlinePickerViewModel
 import com.example.todoAppJpc.utils.deadline.viewModel.TimePickerViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Locale
@@ -20,55 +24,4 @@ class DeadlinePickerViewModelImpl(
 ) : ViewModel(), DeadlinePickerViewModel {
     override val datePickerViewModel get() = _datePickerViewModel
     override val timePickerViewModel get() = _timePickerViewModel
-
-    //        override suspend fun getChipText(): String { //Todo 変更予定の関数名
-    override suspend fun updateDeadlineUiState(): String {
-        /**
-         * Chipを表示させるかどうかを判定する
-         * 一旦初期値を確認したい
-         * 初期値で比較して、もし何も入力されていないなら表示されない。
-         * 入力されているのであれば表示する文字列を返す
-         * 締切日時が入力されるのであれば必ず日付が入力されるんで、日付があるかどうかだけ確認すればいい
-         */
-
-        val userLocale = Locale.getDefault()
-        val cal = Calendar.getInstance()
-
-        val timeFormatter = SimpleDateFormat("HH:mm", userLocale)
-        val timeState = _timePickerViewModel.timePickerState
-        cal.set(Calendar.HOUR_OF_DAY, timeState.hour)
-        cal.set(Calendar.MINUTE, timeState.minute)
-        cal.isLenient = false
-        val timeUiState =
-            if (isShowChip()) timeFormatter.format(
-                cal.time
-            ) else ""
-        val dateFormatter = SimpleDateFormat("MM/dd(EEE)", userLocale)
-
-        try {
-            val setCalApply = viewModelScope.async(Dispatchers.IO) {
-                val dateState = _datePickerViewModel.datePickerState
-                cal.apply {
-                    timeInMillis = dateState.selectedDateMillis!!
-                }
-            }.await()
-            setCalApply
-        } catch (e: Exception) {
-            Log.e("getDeadlineUiState() is error", "$e")
-        }
-
-        val dateUiState =
-            if (isShowChip()) dateFormatter.format(
-                cal.time
-            ) else ""
-
-        return "$dateUiState $timeUiState"
-
-    }
-
-    override fun isShowChip(): Boolean {
-        return _datePickerViewModel.datePickerState.selectedDateMillis!! > 0L
-    }
-
-
 }
--- app/src/main/java/com/example/todoAppJpc/utils/deadline/viewModel/DeadlinePickerViewModel.kt
+++ app/src/main/java/com/example/todoAppJpc/utils/deadline/viewModel/DeadlinePickerViewModel.kt
@@ -6,13 +6,4 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 interface DeadlinePickerViewModel {
     val datePickerViewModel: DatePickerViewModel
     val timePickerViewModel: TimePickerViewModel
-
-    // 設定した日時をChipに表示する関数
-    // override suspend fun getChipText(): String { //Todo 変更予定の関数名
-    suspend fun updateDeadlineUiState(): String
-
-    // Chipを表示させるかどうかを判定する関数
-    fun isShowChip(): Boolean
-
-
 }
