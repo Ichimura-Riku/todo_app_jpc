13388887ec71cfbd807f075a6c253c8844465650
Ichimura-Riku <c1103865@planet.kanazawa-it.ac.jp>
2024-01-15 15:48:31 +0900

    change 登録の時実行するラムダの名前と内容を変更

--- app/src/main/java/com/example/todoAppJpc/ui/home/MainBodyScreen.kt
+++ app/src/main/java/com/example/todoAppJpc/ui/home/MainBodyScreen.kt
@@ -177,18 +177,7 @@ fun MainScreen(
                     // Sheet content
                     TodoEntryBody(
                         // これもいらない説
-                        onSaveClick = {
-                            /**
-                             * Note: If the user rotates the screen very fast, the operation may get cancelled
-                             * and the item may not be saved in the Database. This is because when config
-                             * change occurs, the Activity will be recreated and the rememberCoroutineScope will
-                             * be cancelled - since the scope is bound to composition.
-                             * coroutineScope -> ViewModelScope (ViewModelの中で書いた方がいい)
-                             * この関数が実行される時、deadlinePickerViewModelの値をTodoStateに代入する処理も一緒に行う。
-                             */
-                            coroutineScope.launch {
-                                todoEntryViewModel.adventTodo()
-                            }
+                        closeBottomSheet = {
                             scope.launch { sheetState.hide() }.invokeOnCompletion {
                                 if (!sheetState.isVisible) {
                                     mainBodyViewModel.setShowBottomSheet(false)
