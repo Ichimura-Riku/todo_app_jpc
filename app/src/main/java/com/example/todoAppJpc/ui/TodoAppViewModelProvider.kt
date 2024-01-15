66d8a7a94a3a35ca4ee09f11d12c7b7d377d977e
市村 凌久 <87126275+Ichimura-Riku@users.noreply.github.com>
2024-01-15 16:25:59 +0900

    Update TodoAppViewModelProvider.kt
    clean containerのコメントアウト削除

--- app/src/main/java/com/example/todoAppJpc/ui/TodoAppViewModelProvider.kt
+++ app/src/main/java/com/example/todoAppJpc/ui/TodoAppViewModelProvider.kt
@@ -4,28 +4,5 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
 import com.example.todoAppJpc.MainApplication
 
-//object TodoAppViewModelProvider {
-//    val Factory = viewModelFactory {
-//        initializer {
-//            TodoEntryViewModel(
-//                this.createSavedStateHandle(),
-//                todoApplication().todoContainer.todoRepository,
-//            )
-//        }
-//        initializer {
-//            MainBodyViewModel(
-//                this.createSavedStateHandle(),
-//                todoApplication().todoContainer.todoRepository,
-//            )
-//        }
-//        initializer {
-//            TodoEditViewModel(
-//                this.createSavedStateHandle(),
-//                todoApplication().todoContainer.todoRepository,
-//            )
-//        }
-//    }
-//}
-
 fun CreationExtras.todoApplication(): MainApplication =
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MainApplication)
